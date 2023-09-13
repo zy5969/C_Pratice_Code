3b335f7d016e50de683d515c03430702808ff762
zy <2031769596@qq.com>
2023-09-13 20:51:56 +0800

    C语言字符串

---
+++ 04.HZOJ/166.c
@@ -0,0 +1,33 @@
+#include <string.h>
+#include <stdio.h>
+#define max 10000
+
+int main() {
+    char A[max] = {0}, B[max] = {0};
+    char arr1[max + 5] = {0}, arr2[max + 5] = {0};
+    int N, d = 0;
+    scanf("%s\n%d\n%s", A, &N, B);
+    strcpy(arr1, A);
+    strcpy(arr2, B);
+    int l = strlen(A);
+
+    if(l > 100) printf("100\n");
+    else printf("%d\n", l);
+    l = l + strlen(B);
+    for(int i = l - 1; i > N - 1; i--){
+        arr1[i] = arr1[i - strlen(B)]; 
+    }
+    for(int k = N - 1, j = 0; k < N - 1 + strlen(B), j < strlen(B); k++, j++){
+        arr1[k] = arr2[j];
+    }
+
+    for(int j = l ; j > 0; j--){
+        
+        if(arr1[j] != 'x') d++;
+        else break;
+    }
+    printf("%s\n", arr1);
+    printf("%d\n", d);
+    return 0;
+}
+
