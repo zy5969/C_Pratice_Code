2b718953ac1d13c512559b33fa46a5752bed818f
zy <2031769596@qq.com>
2023-09-21 21:15:45 +0800

    OJ题106

---
+++ 04.HZOJ/106.c
@@ -0,0 +1,14 @@
+//绝对值
+#include<stdio.h>
+#include<math.h>
+
+int main(){
+   double n;
+   scanf("%lf", &n);
+  //  if( n < 0) printf("%d\n", -n);
+   // else printf("%d\n", n);
+    printf("%g\n", fabs(n));
+    return 0;
+}
+
+
