9c12153844b44205ac40e815040f7f7085b43158
zy <2031769596@qq.com>
2023-11-14 20:06:59 +0800

    OJ

---
+++ 04.HZOJ/192.c
@@ -0,0 +1,34 @@
+/*************************************************************************
+	> File Name: 192.c
+	> Author: 
+	> Mail: 
+	> Created Time: Tue 14 Nov 2023 07:55:26 PM CST
+ ************************************************************************/
+
+#include <stdio.h>
+#include <math.h>
+#define e 2.718282
+
+
+
+int fun(int a, double x){
+    x = log(a / x);
+    if(a == 0) x = 0;
+    
+    return fun(a, x);
+   // else if(a / x * 1.0 == pow(e, x)) return x;
+}
+
+
+int main() {
+    int a;
+    double b,c;
+    scanf("%d", &a);
+    b = fun(a, c);
+    
+    printf("%.4lf\n", b);
+    return 0;
+}
+
+
+
