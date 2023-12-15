329688c1347a1ef1b13587e92e0c1eacfa5012f7
zy <2031769596@qq.com>
2023-12-15 23:40:27 +0800

    操作系统

---
+++ common/color.h
@@ -0,0 +1,26 @@
+/*************************************************************************
+	> File Name: color.h
+	> Author: 
+	> Mail: 
+	> Created Time: Fri 15 Dec 2023 07:38:28 PM CST
+ ************************************************************************/
+
+#ifndef _COLOR_H
+#define _COLOR_H
+
+#define NONE "\e[0m" 
+#define BLACK "\e[0;30m"
+#define RED "\e[0;31m"
+#define YELLOW "\e[1;33m"
+#define BLUE "\e[0;34m"
+#define PINK "\e[0;35m"
+#define CYAN "\e[0;36m"
+#define WHITE "\e[1;37m"
+
+#define REVERSE "\e[7m" //背景白，字体黑
+
+#define HIDE "\e[8m" //隐藏
+#define CLEAR "\e[2J" 
+#define CLRLINE "\r\e[k" //清除行
+
+#endif
